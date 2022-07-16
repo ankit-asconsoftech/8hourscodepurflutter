0bf325f67c0baa2de4c89307112d41b6a1214791
Ankit Abishek <ankit@asconsoftech.com>
2022-07-16 11:46:37 +0530

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'home _page.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  //const MyAppp({Key? key}) : super(key: key);
-
+    
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-         child: Container(
-          child: Text("Welcome to GINTAA: A SUSTAINABLE MARKET"),
-        ),
-       ),
-      ),
+      home: HomePage(),
     );
   }
 }
